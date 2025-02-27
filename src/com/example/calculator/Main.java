1b5216c3fcbf4aa58073803a8725368e233ac877
chohyeokjun <chj3731@naver.com>
2025-02-27 17:08:10 +0900

    사칙연산 문자열에서 문자형으로 변환

--- src/com/example/calculator/Main.java
+++ src/com/example/calculator/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 0;
-        int b = 0;
+        int a;
+        int b;
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
