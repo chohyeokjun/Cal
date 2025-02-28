979ddc0ae44dd23901ad3858959dac7d4efb1018
chohyeokjun <chj3731@naver.com>
2025-02-28 10:22:47 +0900

    Exception 이름 변경 및 위치 수정, 계산 여부 확인 yes/exit로 수정

--- src/com/example/calculator/lv1/Calculator.java
+++ src/com/example/calculator/lv1/Calculator.java
@@ -1,8 +1,10 @@
-package com.example.calculator;
+package com.example.calculator.lv1;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-public class Main {
+public class Calculator {
     public static void main(String[] args) {
         int a;
         int b;
@@ -15,26 +17,30 @@ public class Main {
                 a = scanner.nextInt();
 
                 // 숫자가 아닌 다른 값이 입력되면 처음으로 돌아간다.
-            } catch (Exception e) {
+            } catch (InputMismatchException e) {
                 System.out.println("잘못 입력하셨습니다. 숫자를 입력하세요.");
                 scanner.nextLine(); // 버퍼 비우기
                 continue;
             }
 
-            // 사칙연산 부호 선언
+            // 사칙연산 부호 초기화
             scanner.nextLine(); // 버퍼 비우기
-            char operation;
+            char operation = '+';
 
             // 사칙연산 부호 입력
             while (true) {
-                System.out.println("사칙연산 부호(+, -, *, /)를 입력하세요.");
-                operation = scanner.next().charAt(0);
+                try {
+                    System.out.println("사칙연산 부호(+, -, *, /)를 입력하세요.");
+                    operation = scanner.next().charAt(0);
 
-                // 사칙연산 부호가 아닐 시
-                if (operation == ('+') || operation == ('-') || operation == ('*') || operation == ('/')) {
-                    break;
-                } else {
-                    System.out.println("제대로 입력하세요.");
+                    // 사칙연산 부호가 아닐 시
+                    if (operation == ('+') || operation == ('-') || operation == ('*') || operation == ('/')) {
+                        break;
+                    } else {
+                        throw new IllegalArgumentException("부호(+, -, *, /)를 입력하세요.");
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
                 }
             }
 
@@ -62,31 +68,33 @@ public class Main {
                             break;
 
                         default:
-                            System.out.println("잘못된 부호입니다. 정확히 입력하세요!");
+                            // 사실상 필요 없는 부분
+                            throw new IllegalArgumentException("올바른 부호를 입력하세요.");
                     }
 
                     //0으로 나눌 수 없으므로 0이 입력되면 문구가 나오면서 두번째 숫자 입력으로 돌아간다.
                 } catch (ArithmeticException e) {
                     System.out.println("0이 아닌 다른 숫자를 입력하세요.");
-                    // 숫자가 아닌 다른 값이 입력되면 두 번째 숫자 입력으로 돌아간다.
                     continue;
-                } catch (Exception e) {
+                    // 숫자가 아닌 다른 값이 입력되면 두 번째 숫자 입력으로 돌아간다.
+                } catch (InputMismatchException e) {
                     System.out.println("잘못 입력하셨습니다. 숫자를 입력하세요.");
                     scanner.nextLine();
                     continue;
-                } check = false;
+                }
+                check = false;
             }
             // 버퍼 비우기
             scanner.nextLine();
 
             //이후 계산 여부 확인
             while (true) {
-                System.out.println("더 계산하시겠습니까? (y/n)");
+                System.out.println("더 계산하시겠습니까? (yes/exit)");
                 String answer = scanner.nextLine();
 
-                if (answer.equals("y")) {
+                if (answer.equals("yes")) {
                     break;
-                } else if (answer.equals("n")) {
+                } else if (answer.equals("exit")) {
                     System.out.println("계산기를 종료합니다.");
                     return;
                 } else {
